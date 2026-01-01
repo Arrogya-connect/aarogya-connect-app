44b7f0e2647620a74a852339665bf23372ec2839
rohit-kumar0607 <kumarrohit88829@gmail.com>
2026-01-01 22:24:12 +0530

    Fix build: Switch from bcrypt to bcryptjs for Linux compatibility

--- src/seed.js
+++ src/seed.js
@@ -5,7 +5,7 @@
  */
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 const Record = require('./models/Record');
 const Appointment = require('./models/Appointment');
